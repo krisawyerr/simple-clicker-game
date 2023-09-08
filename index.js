e729f655b0ed75d8d108411ee5d6cf9c3f4864ce
krisawyerr <krisawyerrr@gmail.com>
2023-09-08 14:13:43 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// 1 Points 
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function increment1() {
+    count += 25
+    countEl.textContent = count
+}
